d3e00ea204d5109e6477e39e9d38f11ef048ab17
kbutler762 <kbutler762@gmail.com>
2020-07-04 20:45:44 -0400

    Tried running, got error for x$getinverse  that $ operator is invalid for atomic vectors

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,18 @@
 
 ## Write a short comment describing this function
 
+b <- matrix(1:9, nrow=3, ncol = 3)
+
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y){
+                x <<-y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<-inverse
+        getinverse <- function() i
+        list(set=set, get=get, setinverse = setinverse, getinverse=getinverse)
 
 }
 
@@ -11,5 +22,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        i <- x[getinverse]()
+        if(!is.null(i)){
+                message("getting cahced inverse matrix data")
+                return(i)
+        }
+        data <- x[get]()
+        i <- inverse(data,...)
+        x[setinverse](i)
+        i
+        
         ## Return a matrix that is the inverse of 'x'
 }
